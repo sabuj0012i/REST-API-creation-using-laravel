f640c7c916c51766752272643ebfd80532b0a9ce
Sabuj <sabujhossainraj2000@gmail.com>
2026-04-06 23:08:01 +0600

    learning how can i create a group of route which have some prefix

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,4 +10,13 @@ Route::get('/user', function (Request $request) {
 
 Route::get('/hello',function(){
     return["message" => "Hello Laravel API"];
+});
+
+// Route::get('/posts',[PostController::class,'index'])->name('posts.store');
+// Route::post('/posts',[PostController::class,'store'])->name('posts.store');
+// Route::post('/posts',[PostController::class,'store'])->name('posts.store');
+// Route::get('/posts/{id}',[PostController::class,'show'])->name('posts.show');
+
+Route::prefix('v1')->group(function(){
+    Route::apiResource('posts',PostController::class);
 });
